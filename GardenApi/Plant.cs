aaad0e0d8256eb2a5d972c1d460da70f3c984115
Dewi Tjin <6993716+dewitjin@users.noreply.github.com>
2026-02-07 00:51:19 -0800

    Make a comment

--- GardenApi/Plant.cs
+++ GardenApi/Plant.cs
@@ -2,6 +2,9 @@ namespace GardenApi;
 
 public class Plant
 {
+    /// <summary>
+    /// What to call the plant.
+    /// </summary>
     public string? Name { get; set; }
     public int StartMonth { get; set; }
     public int EndMonth { get; set; }
